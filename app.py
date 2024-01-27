32b79a512a15488caf81719d0facac360bfb18fb
Kay Singian <singiankay@gmail.com>
2024-01-27 20:13:26 +0800

    feat: format lint warnings

--- app.py
+++ app.py
@@ -5,30 +5,79 @@ import json
 
 config = dotenv_values('.env')
 client = OpenAI(
-    api_key = config["OPENAI_API_KEY"]
+    api_key=config["OPENAI_API_KEY"]
 )
 
-app = Flask(__name__,
+app = Flask(
+    __name__,
     template_folder='templates',
-    static_url_path='', 
+    static_url_path='',
     static_folder='static'
 )
 
+
 def get_colors(msg):
+    system_content = (
+        "You are a color palette generating assistant that "
+        "responds to text prompts for color palettes "
+        "You should generate color palettes that fit the theme, "
+        "mood, or instructions in the prompt. "
+        "The palettes should be between 2 and 8 colors. "
+        "Desired Format: a JSON array of hexadecimal color codes"
+    )
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": f"""
-             You are a color palette generating assistant that responds to text prompts for color palettes
-             You should generate color palettes that fit the theme, mood, or instructions in the prompt.
-             The palettes should be between 2 and 8 colors.
-             Desired Format: a JSON array of hexadecimal color codes
-             """},
-            {"role": "user", "content": "Convert the following verbal description of a color palette into a list of colors: The Mediterranean Sea"},
-            {"role": "assistant", "content": f"""["#006699", "#66CCCC", "#F0E68C", "#008000", "#F08080"]"""},
-            {"role": "user", "content": "Convert the following verbal description of a color palette into a list of colors: sage, nature, earth"},
-            {"role": "assistant", "content": f"""["#EDF1D6", "#9DC08B", "#609966", "#40513B"]"""},
-            {"role": "user", "content": f"""Convert the following verbal description of a color palette into a list of colors: {msg} """}
+            {
+                "role": "system",
+                "content": system_content
+             },
+            {
+                "role": "user",
+                "content": (
+                    "Convert the following verbal description "
+                    "of a color palette into a list of colors: "
+                    "The Mediterranean Sea"
+                )
+            },
+            {
+                "role": "assistant",
+                "content": (
+                    '['
+                        '"#006699", '
+                        '"#66CCCC", '
+                        '"#F0E68C", '
+                        '"#008000", '
+                        '"#F08080"'
+                    ']'
+                )
+            },
+            {
+                "role": "user",
+                "content": (
+                    "Convert the following verbal description "
+                    "of a color palette into a list of colors: "
+                    "sage, nature, earth"
+                )
+            },
+            {
+                "role": "assistant",
+                "content": (
+                    '['
+                        '"#EDF1D6", '
+                        '"#9DC08B", '
+                        '"#609966", '
+                        '"#40513B"'
+                    ']'
+                )
+            },
+            {
+                "role": "user",
+                "content": (
+                    "Convert the following verbal description "
+                    f"of a color palette into a list of colors: {msg}"
+                )
+            }
         ],
         max_tokens=200
     )
@@ -38,11 +87,13 @@ def get_colors(msg):
 @app.route("/palette", methods=["POST"])
 def get_palette():
     query = request.form.get("query")
-    return { "colors": get_colors(query) }
+    return {"colors": get_colors(query)}
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
